2d3008c070bf231b6462cc0845ba5fa81d8a4ab3
mcelrath <mcelrath>
2001-11-05 05:30:57 +0000

    Converted to hook interface.

--- samples/auto/gag.pl
+++ samples/auto/gag.pl
@@ -2,62 +2,122 @@
 package Gag;
 
 use vars qw(%Gags);  # Variables that will be exported in Gag:: namespace
+use Getopt::Std;
+my($pri) = -2147483646; # lowest possible priority -- let other hooks get it first.
 if(!defined %Gags) {
     %Gags = ();
 }
-&main::save("Gag::Gags", \%Gags);
 
-
-# command '/gag' (list/add/delete gags)
-sub main::dirtcmd_gag {
-    my ($count) = (0);
-    if (/^(\d+)/) {
-        # Remove a certain gag
-        foreach (keys %Gags) {
-            if (++$count == $1) {
-                print "Deleted gag: $_\n";
-                delete $Gags{$_};
-                return;
+&main::run("/hook -T INIT -F -fL perl definegags = Gag::definegags");
+sub definegags {
+    foreach my $name (keys %Gags) {
+        my($hookcmd) = "/hook -T OUTPUT";
+        foreach my $opt (keys %{$Gags{$name}}) {
+            if($Gags{$name}->{$opt} && length($opt) == 1) {
+                if($opt =~ /[iFeDaf]/) { 
+                    $hookcmd .= " -" . $opt;
+                } else {
+                    $hookcmd .= " -" . $opt . " '" . &main::backslashify($Gags{$name}->{$opt}, '\'') . "'";
+                }
             }
         }
-        print "No such gag (there are only $count!) - type gag to see a list.\n";
-    } elsif (/^.{3,}$/) {
-        $Gags{$_} = 0;
-        print "Gagging '$_' from now on.\n" unless $Loading;
-    } else {
-        print "Following gags are active:\n";
-        foreach (keys %Gags) {
-            printf "%2d) $_ (%d)\n", ++$count, $Gags{$_};
-        }
-        print "\nUse gag <string> to add a gag.\n";
-        print "Use gag <number> to remove.\n";
+        $hookcmd .= " " ." -p $pri -fL perl " . $name . " = Gag::gagit";
+        &main::run($hookcmd);
     }
+    &main::run($main::commandCharacter . "hook -d definegags"); # delete myself from INIT list.
+}
+&main::save("Gag::Gags", \%Gags);
+
+sub gagit {
+    $_ = "";
 }
 
-# Run though gags and apply any that are seen
-sub gag_input_hook {
-    foreach $gag (keys %Gags) {
-        if (/$gag/) {
-            $Gags{$gag}++;
-            $_ = "";
-            return 1;
+&main::run("/hook -T COMMAND -fL perl -C gag gag = Gag::command_gag");
+
+# command '/gag' (list/add/delete gags)
+sub command_gag {
+    my(%opts);
+    my(%gaghash);
+    my($name);
+    @ARGV = (); # reset it.
+    if(/${main::commandCharacter}gag(.*)/) { $_ = $1; }
+    else { report_err("This doesn't seem to be a /gag command!\n"); }
+# Line noise counts as perl?  Why, yes.
+    while(/\G\s+(?:(-[A-Za-z]+\s*)?\"([^\"]*?)\"|(-[A-Za-z]+\s*)?\'([^\']*?)\'|([^ \t\n"']+)|(?:\/(.*?(?:\\\\)*)\/))/g) {
+        if(defined $1) { # An option with double-quoted argument following
+            push @ARGV, $1; 
+            push @ARGV, $2;
+        } elsif(defined $2) { # double quoted argument to option without space in front.
+            push @ARGV, $2;
+        } elsif(defined $3) { # An option with single-quoted argument following
+            push @ARGV, $3;
+            push @ARGV, $4;
+        } elsif(defined $4) { # The single-quoted argument.
+            push @ARGV, $4;
+        } elsif(defined $5) { # The rest is the thing to be executed 
+            push @ARGV, $5;
+            if(m/\G\s+(.*)$/g) {
+                push @ARGV, $1;
+            }
+            last;
+        } elsif(defined $6) {
+            push @ARGV, $6;
+        } elsif(defined $7) {
+            push @ARGV, $7;
+        } else {
+            &main::report_err($main::commandCharacter . "gag: Error parsing command into \@ARGV\n");
+            last;
         }
     }
-    return 0;
+    if(defined pos && pos != length) {
+        &main::report_err($main::commandCharacter . "gag: did not reach end of argument string. \n");
+    }
+    getopts('lDFafd:p:c:n:g:L:t:', \%opts);
+    my($fallthrough) = (0);
+    my($hookcmd) = "/hook -T OUTPUT -fL perl -p $pri ";
+
+    if(defined $opts{l} && $opts{l}) {
+        &main::report(sprintf("%-35s%6s%6s %s\n", "Name", "Shots", "Flags", "Groups"));
+        foreach $name (sort keys %Gags) {
+            my($gagref) = $Gags{$name};
+            &main::report(sprintf("%-35s%6s%2s%2s%2s %s\n", $name, $gagref->{n}, 
+                $gagref->{F}?"F":"", $gagref->{D}?"D":"", $gagref->{a}?"C":"", $gagref->{g}));
+            &main::report("\tGagging: ", $gagref->{t});
+        }
+        return 1;
+    }
+    if(defined $opts{d} && $opts{d}) {
+        if(defined $Gags{$opts{d}}) {
+            &main::run($main::commandCharacter . "hook -d " . $opts{d});
+            delete $Gags{$opts{d}};
+        } else {
+            &main::report_err($main::commandCharacter . "gag: cannot delete gag '$opts{d}' because it isn't defined.\n");
+        }
+        return 1;
+    }
+    if(defined $opts{a}) { $hookcmd .= "-a "; $gaghash{a} = 1; }
+    else { $gaghash{a} = 0; }
+    if(defined $opts{D}) { $hookcmd .= "-D "; $gaghash{D} = 1; }
+    else { $gaghash{D} = ""; }
+    if(defined $opts{n}) { $hookcmd .= "-n '$opts{n}' "; $gaghash{n} = $opts{n}; }
+    else { $gaghash{n} = -1; } # default shots = infinite
+    if(defined $opts{g}) { $hookcmd .= "-g '$opts{g}' "; $gaghash{g} = $opts{g}; }
+    else { $gaghash{g} = ""; }
+    if(defined $opts{t}) { $hookcmd .= "-t '$opts{t}' "; $gaghash{t} = $opts{t}; }
+    else { $gaghash{t} = ""; }
+
+    if($#ARGV < 0) { # /gag was dropped already.
+        &main::report_err($main::commandCharacter . "gag: Not enough arguments.  See /help gag.");
+        for($i=0;$i<$#ARGV;$i++) { &main::report_err("\t$i: $ARGV[$i]\n"); }
+        return 1;
+    }
+    $name = $ARGV[0];
+    $hookcmd .= "'" . $name . "' = Gag::gagit";
+#    &main::report("hook command is: $hookcmd");
+    &main::run($hookcmd);
+    $Gags{$name} = \%gaghash;  # main::save will save complex data structures for us!
+    return 1;
 }
-
-# save gags to config file.
-#sub save_gags {
-#    local (*FH) = $_[0];
-#    foreach (keys %Gags) {
-#        print FH "Gag $_\n";
-#    }
-#}
-
-#&main::hook_add("output", "gag_input_hook", \&gag_input_hook, 0, 0);
-&main::run($main::commandCharacter . "hook -T OUTPUT -F -fL perl gag_input = Gag::gag_input_hook");
-#save_add(\&save_gags);
-#load_add("gag", \&dirtcmd_gag);
 
 print "Loaded auto/gag.pl\t(Routines to gag mud output)\n";
 
